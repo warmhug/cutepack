1c698e22c1541f6a7b3cdc1096529446726a0220
hualei.hl <hualei.hl@alibaba-inc.com>
2014-10-10 12:20:19 +0800

    up

--- test/in/tin.js
+++ test/in/tin.js
@@ -1,8 +1,4 @@
-/****************************************************
-* author：  wb-hualei.h
-* time：    2013/6/25 11:42:15
-* fileName：tin.js
-*****************************************************/
+
 function aaaa(){
 	alert('ininin')
 	console.log('ininin')
